4393976fa4bf323d5a0c6874a3a80ed6d98b34ca
JenniD5 <elizabethduran0105@gmail.com>
2019-08-23 08:45:36 -0700

    Agregar archivos de proyecto.

---
+++ ColeccionesAlumno/Grupo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColeccionesAlumno
+{
+    class Grupo
+    {
+        public string Materia { get; set; } 
+        public string Codigo { get; set; }
+
+        public List <Alumno> Alumnos { get; set; }
+
+
+        public Grupo(string materia, string codigo)
+        {
+            Materia = materia;
+            Codigo = codigo;
+
+            Alumnos = new List<Alumno>(); // para que cree una lista nueva, se inicializa para que tenga (se le asigne) un espacio de memoria y no se quede en nulo 
+
+        }
+
+    }
+}
---
+++ ColeccionesAlumno/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColeccionesAlumno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Grupo> grupos = new List<Grupo>();
+
+            //son variable aisladas
+            Grupo matematicas = new Grupo("Matematicas", "cdmat5736");//dar de alta un gurpo
+            Grupo historia = new Grupo("historia", "h5783");
+
+            //añadir a la lista, las variables anteriores 
+            grupos.Add(matematicas);
+            grupos.Add(historia);
+
+            Alumno juan = new Alumno("juan", "89543");
+            Alumno maria = new Alumno("maria", "79y857439");
+            Alumno lili = new Alumno("lili", "89ln9");
+            Alumno ana = new Alumno("ana", "a89889");
+            Alumno victor = new Alumno("victor", "u4892");
+
+
+            historia.Alumnos.Add(juan);//para agregar a la lista 
+            historia.Alumnos.Add(lili);
+            historia.Alumnos.Add(maria);
+
+            matematicas.Alumnos.Add(ana);
+            matematicas.Alumnos.Add(victor);
+            matematicas.Alumnos.Add(lili);
+            matematicas.Alumnos.Add(juan);
+            matematicas.Alumnos.Add(maria);
+           
+
+            foreach(Grupo grupo in grupos)//declarar una variable, para cada grupo en la coleccion gurpo hace lo siguiente, el primero es le tipo de dato
+
+            {
+                Console.WriteLine("grupo:" + grupo.Codigo);
+                Console.WriteLine("materia:" + grupo.Materia);
+                Console.WriteLine("lista de alumnos");
+
+                foreach(Alumno alumno in grupo.Alumnos)
+                {
+                    Console.WriteLine("*" + alumno.Nombre);
+                    Console.WriteLine("        -"+alumno.Matricula);          
+                }
+            }
+
+
+            Console.Read();
+
+
+
+        }
+    }
+}
